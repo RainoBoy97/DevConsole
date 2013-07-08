5c0e2e2947a6c70e3d116b42d51793a7fb0ee63c
Shockzz <projectshockzz@hotmail.co.uk>
2013-07-08 21:54:00 +0100

    Create a window.

--- src/me/rainoshockzz/devconsole/DevConsole.java
+++ src/me/rainoshockzz/devconsole/DevConsole.java
@@ -1,7 +1,24 @@
 package me.rainoshockzz.devconsole;
 
+import javax.swing.JFrame;
+
 /** DevConsole - Developed by RainoBoy97 and XX_iiShockzz_XX **/
 
-public class DevConsole {
+public class DevConsole extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+
+	private static void createFrame() {
+		JFrame frame = new JFrame("DevConsole");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setResizable(false);
+		frame.setSize(800, 600);
+		frame.setVisible(true);
+		frame.setLocationRelativeTo(null);
+	}
+
+	public static void main(String[] args) {
+		createFrame();
+	}
 
 }
