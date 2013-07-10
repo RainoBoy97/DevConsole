a19149d35818bb0a8e2d86bf882f3e90e2d66336
RainoBoy97 <rainoboy97@gmail.com>
2013-07-10 19:19:03 +0200

    Moved the GUI to another class

--- src/me/rainoshockzz/devconsole/DevConsole.java
+++ src/me/rainoshockzz/devconsole/DevConsole.java
@@ -1,24 +1,13 @@
 package me.rainoshockzz.devconsole;
 
-import javax.swing.JFrame;
-
 /** DevConsole - Developed by RainoBoy97 and XX_iiShockzz_XX **/
 
-public class DevConsole extends JFrame {
-
-	private static final long serialVersionUID = 1L;
-
-	private static void createFrame() {
-		JFrame frame = new JFrame("DevConsole");
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setResizable(false);
-		frame.setSize(800, 600);
-		frame.setVisible(true);
-		frame.setLocationRelativeTo(null);
-	}
+public class DevConsole {
+	
+	public static final String NAME = "DevConsole";
 
 	public static void main(String[] args) {
-		createFrame();
+		Window gui = new Window();
 	}
 
 }
