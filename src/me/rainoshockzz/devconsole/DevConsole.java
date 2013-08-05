5386722fc5b632ff266ec955dd99d76f45a59b81
RainoBoy97 <rainoboy97@gmail.com>
2013-08-05 16:06:18 +0200

    ok

--- src/me/rainoshockzz/devconsole/DevConsole.java
+++ src/me/rainoshockzz/devconsole/DevConsole.java
@@ -1,13 +1,29 @@
 package me.rainoshockzz.devconsole;
 
+import java.awt.Dimension;
+
+import javax.swing.SwingUtilities;
+
 /** DevConsole - Developed by RainoBoy97 and XX_iiShockzz_XX **/
 
 public class DevConsole {
-	
+
 	public static final String NAME = "DevConsole";
 
+	public static final Dimension CONSOLE_SIZE = new Dimension(700, 500);
+
 	public static void main(String[] args) {
-		Window gui = new Window();
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Window frame = new Window();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+
 	}
 
 }
--- src/me/rainoshockzz/devconsole/Window.java
+++ src/me/rainoshockzz/devconsole/Window.java
@@ -1,18 +1,51 @@
 package me.rainoshockzz.devconsole;
 
+import java.awt.Button;
+import java.awt.Color;
+import java.awt.TextArea;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
 
 public class Window extends JFrame {
-	
+
 	private static final long serialVersionUID = 1L;
-
+	
+	private JPanel contentPane;
+	
 	public Window() {
-	    this.setTitle(DevConsole.NAME);
-	    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	    this.setResizable(false);
-	    this.setSize(800, 600);
-            this.setVisible(true);
-            this.setLocationRelativeTo(null);
+		super(DevConsole.NAME);
+		setResizable(false);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 800, 600);
+		contentPane = new JPanel();
+		contentPane.setBackground(Color.WHITE);
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		TextArea console = new TextArea();
+		console.setEditable(false);
+		console.setBounds(10, 10, 600, 552);
+		contentPane.add(console);
+		
+		Button aboutBtn = new Button("About");
+		aboutBtn.setBounds(741, 540, 43, 22);
+		aboutBtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				JOptionPane.showMessageDialog(null, "DevConsole is an external developer console for Minecraft 1.6.0 and higher." +
+						"\n" +
+						"With more features than the normal developer console built into the new Minecraft launcher." +
+						"\n\n" +
+						"Coded by RainoBoy97 and XX_iiShockzz_XX",
+						"DevConsole",
+						JOptionPane.INFORMATION_MESSAGE);
+			}
+		});
+		contentPane.add(aboutBtn);
 	}
-
 }
